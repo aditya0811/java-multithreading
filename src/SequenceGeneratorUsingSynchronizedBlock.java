82394462fb0e5ca1fdacde997014e8a1ba29740a
Aditya Mahajan <aditya.m.1997@gmail.com>
2025-02-10 23:49:52 +0530

    3 types of mutex

---
+++ src/SequenceGeneratorUsingSynchronizedBlock.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class SequenceGeneratorUsingSynchronizedBlock extends SequenceGenerator {
+
+  private final Object mutex = new Object();
+  private final Object mutex2 = new ReentrantLock();
+
+  //Semaphore allows 3 threads at max to access
+  private final Object mutex3 = new Semaphore(3);
+  @Override
+  public int getNextSequence() {
+    synchronized (mutex) {
+      return super.getNextSequence();
+    }
+
+  }
+}
