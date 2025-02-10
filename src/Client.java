2d5b4cbf00a274dc7ff09a1c898521add30d959c
Aditya Mahajan <aditya.m.1997@gmail.com>
2025-02-10 23:35:31 +0530

    Initial commit
    Race condition, here sout will give set.size<count, as multiple threads would have updated value from x->y, not allowing the program to reach count

---
+++ src/Client.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+
+public class Client {
+  public static void main(String[] args) throws Exception {
+    int count = 1000;
+    Set<Integer> uniqueSequences = getUniqueSequences(new SequenceGenerator(),count);
+    System.out.println("This is set size:" + uniqueSequences.size());
+  }
+
+  private static Set<Integer> getUniqueSequences(SequenceGenerator sequenceGenerator, int count)
+  throws Exception{
+    Set<Integer> uniqueSequences = new LinkedHashSet<>();
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    sequenceGenerator.getNextSequence();
+    List<Future<Integer>> futures = new ArrayList<>();
+    for(int i=0;i<count;i++) {
+      Future future = executorService.submit(sequenceGenerator::getNextSequence);
+      futures.add(future);
+    }
+    for(int i=0;i<count;i++) {
+      uniqueSequences.add(futures.get(i).get());
+    }
+    executorService.awaitTermination(1, TimeUnit.SECONDS);
+    executorService.shutdown();
+    return uniqueSequences;
+
+  }
+}
---
+++ src/SequenceGenerator.java
@@ -0,0 +1,7 @@
+public class SequenceGenerator {
+  private int currentValue = 0;
+  public int getNextSequence() {
+    currentValue = currentValue + 1;
+    return currentValue;
+  }
+}
